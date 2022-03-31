9b486e11c5d9547b0a5c487b8ae07d1c12dfb153
PaSeRouS <paser96@mail.ru>
2022-03-31 23:51:13 +0300

    Функция add_sub_to_gsheet()
    Функция, которая добавляет информацию о подписке в гугл документ

--- gsheets_functions.py
+++ gsheets_functions.py
@@ -3,12 +3,36 @@ import os
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
-def add_to_gsheet(first_name, last_name=''):
+
+def init():
     gscope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
     gcredentials = os.getenv('GOOGLE_SHEETS_KEY')
-    gdocument = 'foodPlan'
     credentials = ServiceAccountCredentials.from_json_keyfile_name(gcredentials, gscope)
-    gc = gspread.authorize(credentials)
+    return gspread.authorize(credentials)
+
+def add_to_gsheet(first_name, last_name=''):
+    gdocument = 'foodPlan'
+    gc = init()
     wks = gc.open(gdocument).sheet1
     wks.append_row([first_name, last_name])
+
+
+def add_sub_to_gsheet(first_name, sub_parameters, last_name=''):
+    gdocument = 'foodPlan'
+    gc = init()
+    wks = gc.open(gdocument).worksheet('Лист2')
+
+    users_allergies = ''
+    for allergy in sub_parameters['allergies']:
+        if users_allergies:
+            users_allergies = users_allergies + ', ' + allergy
+        else:
+            users_allergies = allergy
+
+    print(sub_parameters)
+
+    wks.append_row([first_name, last_name,
+                    sub_parameters['menu_type'], sub_parameters['number_of_meals'],
+                    sub_parameters['number_of_persons'], users_allergies,
+                    sub_parameters['type_of_subs'], sub_parameters['promo_code'],])
     
