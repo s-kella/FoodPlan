42055d06ee74146b82fff7ee2669efffb5f24be5
PaSeRouS <paser96@mail.ru>
2022-03-31 14:55:35 +0300

    Создал файл для работы с гугл доком
    В нём будут все функции, которые работают непосредственно с гугл доком

---
+++ gsheets_functions.py
@@ -0,0 +1,13 @@
+import os
+
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
+def add_to_gsheet(first_name, last_name=''):
+    gscope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
+    gcredentials = os.getenv('GOOGLE_SHEETS_KEY')
+    gdocument = 'foodPlan'
+    credentials = ServiceAccountCredentials.from_json_keyfile_name(gcredentials, gscope)
+    gc = gspread.authorize(credentials)
+    wks = gc.open(gdocument).sheet1
+    wks.append_row([first_name, last_name])
