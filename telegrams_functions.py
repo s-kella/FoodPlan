02141a57c5db76f201e90308b1a7ecbdd2ff3751
PaSeRouS <paser96@mail.ru>
2022-04-01 16:16:21 +0300

    Доработки кнопки "Оформить подписку"
    1. В некоторых пунктах сделал строки по три кнопки в каждой.
    2. Добавил часть про промокод.
    3. Можно выбрать несколько аллергий, либо отсутствие аллергий.

--- telegrams_functions.py
+++ telegrams_functions.py
@@ -5,12 +5,15 @@ from telegram.ext import MessageHandler, Filters
 from gsheets_functions import add_to_gsheet, add_sub_to_gsheet
 
 
+has_promo_code = False
+
 sub_parameters = {
         'menu_type': '',
         'number_of_meals': '',
         'number_of_persons': '',
         'allergies': [],
         'type_of_subs': '',
+        'price': 0,
         'promo_code': ''
     }
 
@@ -65,7 +68,6 @@ def set_keyboards_buttons(buttons):
 
     return keyboard
 
-
 def run_initial_keyboard(query):
     start_buttons = ['Мои подписки', 'Оформить подписку']
 
@@ -78,65 +80,126 @@ def run_initial_keyboard(query):
     )
 
 
-def message_handler(update, callback_context):
-    global sub_parameters
+def message_handler(update, context):
+    global sub_parameters, has_promo_code
 
     text = update.message.text
     user = update.message.from_user
 
     my_subs_buttons = ['Вернуться']
     start_buttons = ['Мои подписки', 'Оформить подписку']
-    menu = ['Классическое', 'Низкоуглеводное', 'Вегетарианское', 'Кето']
-    number_of_meals = ['1 раз в день', '2 раза в день', '3 раза в день', '4 раза в день', '5 раз в день', '6 раз в день']
-    number_of_persons = ['1 персона', '2 персоны', '3 персоны', '4 персоны', '5 персон', '6 персон']
-    allergies = ['Рыба и морепродукты', 'Мясо', 'Зерновые', 'Продукты пчеловодства', 'Орехи и бобовые', 'Молочные продукты']
-    type_of_subs = ['1 день (19р)', '7 дней (59р)', '30 дней(199р)', '120 дней(599р)', '180 дней(799р)', '365 дней(999р)']
+    menu_buttons = ['Классическое', 'Низкоуглеводное', 'Вегетарианское', 'Кето']
+    number_of_meals_buttons = ['1 раз в день', '2 раза в день', '3 раза в день', '4 раза в день', '5 раз в день', '6 раз в день']
+    number_of_persons_buttons = ['1 персона', '2 персоны', '3 персоны', '4 персоны', '5 персон', '6 персон']
+    allergies_buttons = ['Рыба и морепродукты', 'Мясо', 'Зерновые', 'Продукты пчеловодства', 'Орехи и бобовые', 'Молочные продукты', 'Аллергий нет', 'Больше аллергий нет']
+    type_of_subs_buttons = ['1 день (19р)', '7 дней (59р)', '30 дней(199р)', '120 дней(599р)', '180 дней(799р)', '365 дней(999р)']
+    promo_code_buttons = ['Да, есть', 'Нет']
     pay_buttons = ['Оплатить']
 
     if text == 'Мои подписки':
         reply_markup = get_keyboard(my_subs_buttons)
         message = 'На данный момент подписок нет'
     elif text == 'Оформить подписку':
-        reply_markup = get_keyboard(menu)
+        reply_markup = get_keyboard(menu_buttons)
         message = 'Выберите тип меню'
-    elif text in menu:
+    elif text in menu_buttons:
         sub_parameters['menu_type'] = text
 
-        reply_markup = get_keyboard(number_of_meals)
+        reply_markup = ReplyKeyboardMarkup (
+            keyboard=[
+                ['1 раз в день', '2 раза в день', '3 раза в день'],
+                ['4 раза в день', '5 раз в день', '6 раз в день']
+            ],
+            resize_keyboard=True,
+        )
+
         message = 'Выберите количество приёмов пищи'
-    elif text in number_of_meals:
+    elif text in number_of_meals_buttons:
         sub_parameters['number_of_meals'] = text
 
-        reply_markup = get_keyboard(number_of_persons)
+        reply_markup = ReplyKeyboardMarkup (
+            keyboard=[
+                ['1 персона', '2 персоны', '3 персоны'],
+                ['4 персоны', '5 персон', '6 персон']
+            ],
+            resize_keyboard=True,
+        )
+
         message = 'Выберите количество персон'
-    elif text in number_of_persons:
+    elif text in number_of_persons_buttons:
         sub_parameters['number_of_persons'] = text
+        
+        reply_markup = ReplyKeyboardMarkup (
+            keyboard=[
+                ['Рыба и морепродукты', 'Мясо', 'Зерновые'],
+                ['Продукты пчеловодства', 'Орехи и бобовые', 'Молочные продукты'],
+                ['Аллергий нет']
+            ],
+            resize_keyboard=True,
+        )
 
-        reply_markup = get_keyboard(allergies)
         message = 'Аллергии'
-    elif text in allergies:
-        sub_parameters['allergies'].append(text)
+    elif text in allergies_buttons:
+        if text == 'Аллергий нет' or text == 'Больше аллергий нет':
+            reply_markup = ReplyKeyboardMarkup (
+                keyboard=[
+                    ['1 день (19р)', '7 дней (59р)', '30 дней(199р)'],
+                    ['120 дней(599р)', '180 дней(799р)', '365 дней(999р)']
+                ],
+                resize_keyboard=True,
+            )
+            
+            message = 'Срок подписки'
+        else:
+            sub_parameters['allergies'].append(text)
 
-        reply_markup = get_keyboard(type_of_subs)
-        message = 'Срок подписки'
-    elif text in type_of_subs:
+            reply_markup = ReplyKeyboardMarkup (
+                keyboard=[
+                    ['Рыба и морепродукты', 'Мясо', 'Зерновые'],
+                    ['Продукты пчеловодства', 'Орехи и бобовые', 'Молочные продукты'],
+                    ['Больше аллергий нет']
+                ],
+                resize_keyboard=True,
+            )
+
+            message='Есть ли у вас ещё аллергии?'
+
+    elif text in type_of_subs_buttons:
         sub_parameters['type_of_subs'] = text
 
         if text == '1 день (19р)':
-                sum = 19
+                sub_parameters['price'] = 19
         elif text == '7 дней (59р)':
-                sum = 59
+                sub_parameters['price'] = 59
         elif text == '30 дней(199р)':
-                sum = 199
+                sub_parameters['price'] = 199
         elif text == '120 дней(599р)':
-                sum = 599
+                sub_parameters['price'] = 599
         elif text == '180 дней(799р)':
-                sum = 799
+                sub_parameters['price'] = 799
         elif text == '365 дней(999р)':
-                sum = 999
+                sub_parameters['price'] = 999
+
+        reply_markup = get_keyboard(promo_code_buttons)
+        message = f'Есть ли у вас промокод?'
+
+    elif text in promo_code_buttons:
+        if text == 'Да, есть':
+            has_promo_code = True
+
+            reply_markup = ReplyKeyboardRemove()
+            message = 'Напишите, пожалуйста, промокод'
+        else:
+            reply_markup = get_keyboard(pay_buttons)
+            message = f'Оплатите Вашу подписку стоимостью {sub_parameters["price"]}р.'
+
+    elif text and has_promo_code:
+        sub_parameters['promo_code'] = text
+        sub_parameters['price'] = int(sub_parameters['price'] * 0.8)
+        has_promo_code = False
 
         reply_markup = get_keyboard(pay_buttons)
-        message = f'Оплатите Вашу подписку стоимостью {sum}р.'
+        message = f'Оплатите Вашу подписку стоимостью {sub_parameters["price"]}р.'
 
     elif text in pay_buttons:
         reply_markup = get_keyboard(start_buttons)
@@ -156,8 +219,10 @@ def message_handler(update, callback_context):
             'number_of_persons': 0,
             'allergies': [],
             'type_of_subs': '',
+            'price': 0,
             'promo_code': ''
         }
+
     elif text in my_subs_buttons:
         reply_markup = get_keyboard(start_buttons)
         message = 'Выберите действие:'
